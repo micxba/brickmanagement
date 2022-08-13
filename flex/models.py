622184bb8b84e2623883b81a1ec7d116e8113651
Michael Baumann <mb@baumannlive.com>
2022-08-13 10:41:42 -0400

    Basic Flex Page Model

--- flex/models.py
+++ flex/models.py
@@ -1,3 +1,21 @@
+""" Flexible Pages """
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+
+class FlexPage(Page):
+    template = "flex/flex_page.html"
+
+    # @todo add streamFields
+    #content = StreamField()
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
+
+    class Meta:
+        verbose_name = "Flexible Page"
+        verbose_name_plural = "Flexible Pages"
