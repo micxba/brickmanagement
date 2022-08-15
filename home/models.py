c14ec5eb15fa03d0926a92fe2f50e79ce5d73b5c
Michael Baumann <mb@baumannlive.com>
2022-08-15 14:40:55 -0400

    Image Carousel for Homepage

--- home/models.py
+++ home/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
-from wagtail.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, PageChooserPanel, InlinePanel
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 
 from streams import blocks
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for a HomePage carousel"""
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,      #No Cascading
+        related_name="+"
+    )
+
+    panels = [
+        ImageChooserPanel("carousel_image")
+    ]
 
 
 class HomePage(Page):
@@ -44,12 +61,18 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_sitename"),
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
+        MultiFieldPanel([
+            FieldPanel("banner_sitename"),
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image"),
+            PageChooserPanel("banner_cta"),
+        ], heading="Banner Options"),
+        MultiFieldPanel([
+            InlinePanel("carousel_images", max_num=5, label="Image"),
+        ], heading="Carousel Images"),
         StreamFieldPanel("content"),
+        
     ]
     class Meta:
         verbose_name = "Home Page"
